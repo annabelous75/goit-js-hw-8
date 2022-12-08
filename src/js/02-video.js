5f40c871129bb2e4baa533909cd24557d9ff4f4d
annabelous75 <57856008+annabelous75@users.noreply.github.com>
2022-12-08 19:43:28 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,19 @@ import Player from '@vimeo/player';
 const LOCALSTORAGE = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.setCurrentTime(localStorage.getItem('setCurrentTime(|)') || 0);
+let timeupdate=localStorage.getItem(LOCALSTORAGE);
+player.on('play', function(){
+    console.log('video played');
+});
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-    localStorage.setItem('LOCALSTORAGE', seconds);
-}
+function onPlay() {
 
+    player.getCurrentTime().then(
+        function(seconds){
+            localStorage.setItem(LOCALSTORAGE, seconds);
+});
+
+}
+player.setCurrentTime(localStorage.getItem(LOCALSTORAGE)||0);
